d5399a04c73dfb53361d41e4664805649259109f
Jessica <jessica@insaite.io>
2018-12-27 12:22:57 -0600

    Primer commit

---
+++ server/config/config.js
@@ -0,0 +1,11 @@
+//process siempre esta corriendo 
+//objeto global y es actualizado dependiendo de eltorno donde se desarrollle
+
+
+
+
+/////////////////////////////////////
+// Puerto
+////////////////////////////////////
+
+process.envPORT = process.env.PORT || 3000;
